075d247572d6bdfa355437302c78f22d33c1f123
Kimbsy <lordkimber@gmail.com>
2019-08-14 22:31:27 +0100

    camera following even cooler

--- src/main.js
+++ src/main.js
@@ -64,7 +64,7 @@ function create () {
     this.input.keyboard.on('keydown-SPACE', attemptJumpThrow, null);
 
     this.cameras.main.setSize(screenWidth, screenHeight);
-    this.cameras.main.startFollow(player, 1, 0.1, 0.1);
+    this.cameras.main.startFollow(player, 1, 0.04, 0.04);
 }
 
 function stickToWall() {
